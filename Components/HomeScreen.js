ba49e96aad4916b7ae43b87ed7be01aa561c6bb5
Sam <samblackburn2828@gmail.com>
2020-11-02 10:03:09 +0000

    Home Page added

---
+++ Components/HomeScreen.js
@@ -0,0 +1,13 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
