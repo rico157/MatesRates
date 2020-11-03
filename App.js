20c201946f3632c0b625def42b8985a78c91df0c
Sam <samblackburn2828@gmail.com>
2020-11-03 11:13:09 +0000

    styling

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer, TabActions } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './Components/HomeComponents/HomeScreen';
-import ProfileScreen from './Components/ProfileComponents/ProfileScreen';
-import MapView from './Components/MapViewComponents/MapView';
-import SearchBar from './Components/SearchBarComponents/SearchBar';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer, TabActions } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./Components/HomeComponents/HomeScreen";
+import ProfileScreen from "./Components/ProfileComponents/ProfileScreen";
+import MapView from "./Components/MapViewComponents/MapView";
+import SearchBar from "./Components/SearchBarComponents/SearchBar";
 
 const Tab = createBottomTabNavigator();
 
@@ -26,12 +26,12 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   Header: {
-    color: 'red',
+    color: "red",
   },
 });
 
--- Components/SharedComponents/RestaurantList.js
+++ Components/SharedComponents/RestaurantList.js
@@ -1,28 +1,28 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
-import RestaurantCard from './RestaurantCard';
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import RestaurantCard from "./RestaurantCard";
 
 class RestaurantList extends Component {
   state = {
     isLoading: null,
     restaurants: [
       {
-        name: 'Slug & Lettuce',
+        name: "Slug & Lettuce",
         rating: 4.5,
-        address: '123 Real Address Lane',
+        address: "123 Real Address Lane",
         id: 1,
       },
       {
-        name: 'Turtle Bay',
+        name: "Turtle Bay",
         rating: 3,
-        address: '100 Oxford Street',
+        address: "100 Oxford Street",
         id: 2,
       },
       {
-        name: 'Rudys Pizza',
+        name: "Rudys Pizza",
         rating: 4.8,
-        address: '25 Oxford Street',
+        address: "25 Oxford Street",
         id: 3,
       },
     ],
@@ -35,7 +35,7 @@ class RestaurantList extends Component {
           data={this.state.restaurants}
           renderItem={({ item }) => (
             <View style={styles.card}>
-              <Text>{item.name}</Text>
+              <Text style={styles.name}>{item.name}</Text>
               <Text>{item.address}</Text>
               <Text>{item.rating}</Text>
             </View>
@@ -49,12 +49,17 @@ class RestaurantList extends Component {
 
 const styles = StyleSheet.create({
   card: {
-    fontSize: '20px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    border: '1px solid black',
-    margin: '15px',
+    fontSize: "20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid darkblue",
+    marginTop: "15px",
+    borderLeft: "0.25",
+    borderRight: "0.25",
+  },
+  name: {
+    fontSize: "30px",
   },
 });
 
