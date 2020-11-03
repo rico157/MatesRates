1efbd7e87ded4eb1691426da5a1ebd96cc48323e
hjdisley <hjdisley@gmail.com>
2020-11-03 14:55:31 +0000

    added to restaurant list

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import "react-native-gesture-handler";
-import * as React from "react";
-import { NavigationContainer, TabActions } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./Components/HomeComponents/HomeScreen";
-import ProfileScreen from "./Components/ProfileComponents/ProfileScreen";
-import MapView from "./Components/MapViewComponents/MapView";
-import SearchBar from "./Components/SearchBarComponents/SearchBar";
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './Components/HomeComponents/HomeScreen';
+import ProfileScreen from './Components/ProfileComponents/ProfileScreen';
+import MapView from './Components/MapViewComponents/MapView';
+import SearchBar from './Components/SearchBarComponents/SearchBar';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,8 +15,8 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="SearchBar" component={SearchBar} />
-        <Tab.Screen name="MapView" component={MapView} />
+        <Tab.Screen name="Search" component={SearchBar} />
+        <Tab.Screen name="Map" component={MapView} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
@@ -26,12 +26,12 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   Header: {
-    color: "red",
+    color: 'red',
   },
 });
 
