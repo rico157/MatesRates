ee825ed1b6ce39127d728c9cf3a2e74174bb2cf7
Sam <samblackburn2828@gmail.com>
2020-11-12 09:24:11 +0000

    some styling components

--- Components/navigators/HomeScreenNavigator.js
+++ Components/navigators/HomeScreenNavigator.js
@@ -1,19 +1,32 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import Restaurant from '../screens/RestaurantPage';
-import ReviewForm from '../screens/ReviewFormPage';
-import Home from '../screens/HomePage';
-import { useQuery, gql } from '@apollo/client';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Restaurant from "../screens/RestaurantPage";
+import ReviewForm from "../screens/ReviewFormPage";
+import Home from "../screens/HomePage";
+import { useQuery, gql } from "@apollo/client";
+import LogoTitle from "../common/LogoTitle";
 
 const Stack = createStackNavigator();
 
 const HomeScreenNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home}></Stack.Screen>
-      <Stack.Screen name="Restaurant" component={Restaurant}></Stack.Screen>
-      <Stack.Screen name="ReviewForm" component={ReviewForm}></Stack.Screen>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="Restaurant"
+        component={Restaurant}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="ReviewForm"
+        component={ReviewForm}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      ></Stack.Screen>
     </Stack.Navigator>
   );
 };
---
+++ Styling/global-style.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 20,
+    fontFamily: "Changa One",
+    fontWeight: "Bold",
+    color: "0F0508",
+    alignContent: "center",
+  },
+  contentText: {
+    fontSize: 15,
+    fontFamily: "Changa",
+    color: "0F0508",
+    alignContent: "center",
+  },
+  cardContainer: {
+    backgroundColor: "F8A677",
+    alignContent: "center",
+    flex: 1,
+  },
+  imageContainer: {
+    resizeMode: "contain",
+  },
+  appButtonContainer: {
+    backgroundColor: "4E2D3E",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+});
+
+// Family font: Changa
+// Colour scheme: FAA275, FF8C61, 5C374C
+// Header: Changa One
+// ScrollView components
+// Title Text Components
+// Button Components
+export default styles;
